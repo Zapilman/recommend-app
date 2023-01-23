2aadef0f3bd3f48de7be21c37be877865361c373
Zapilman <17uraz12@gmail.com>
2023-01-23 15:15:34 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Button from './UI/Button/Button';
+import './style.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button />
+      <div>some </div>
+    </div>
+  );
+};
+export default App;
---
+++ src/UI/Button/Button.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Button = () => {
+  return <div>some shit</div>
+}
+
+export default Button
