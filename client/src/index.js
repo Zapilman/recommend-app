9de6d19d02263bc3e101f7fd9f0ea37dd17951bc
Zapilman <17uraz12@gmail.com>
2023-01-29 10:41:46 +0200

    add Apollo for client

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,9 @@
+import {
+  ApolloClient,
+  ApolloProvider,
+  gql,
+  InMemoryCache,
+} from '@apollo/client';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
@@ -6,12 +12,19 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { store } from './store/store';
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql',
+  cache: new InMemoryCache(),
+});
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ApolloProvider>
   </BrowserRouter>,
 );
